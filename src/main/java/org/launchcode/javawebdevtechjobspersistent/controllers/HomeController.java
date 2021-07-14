a3e607423212a313d1f36f146bc16b4f003c6ef9
MBeckett <maddbeck@gmail.com>
2021-07-13 21:58:05 -0500

    Step Four Home Controller ProcessJobForm

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/controllers/HomeController.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/controllers/HomeController.java
@@ -13,7 +13,6 @@ import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -48,51 +47,61 @@ public class HomeController {
         return "add";
     }
 
-//THIS CONTROLLER METHOD IS WHERE THE PROBLEM LIES
-//NO JOB OBJECTS BEING CREATED & NOTHING IS GOING INTO THE JOB REPO
-//    Add a @RequestParam to this method?
     @PostMapping("add")
     public String processAddJobForm(@ModelAttribute @Valid Job newJob,
-                                    Errors errors, Model model, @RequestParam int employerId,
+                                    Errors errors, Model model, @RequestParam Integer employerId,
                                     @RequestParam List<Integer> skills){
         if (errors.hasErrors()) {
             model.addAttribute("title", "Add Job");
-            model.addAttribute(new Job());
             return "add";
         }
-
+//        if (employerId == null) {
+//            model.addAttribute("title", "All Jobs");
+//            model.addAttribute("jobs", jobRepository.findAll());
+//            return "add";
+//        } else {
+//            Optional<Employer> result = employerRepository.findById(employerId);
+//            if (result.isEmpty()) {
+//                model.addAttribute("title", "Invalid Employer ID: " + employerId);
+//            } else {
+//                Employer employer = result.get();
+//                model.addAttribute("title", "Jobs with Employer: " + employer.getName());
+//                model.addAttribute("jobs", employer.getJobs());
+//            }
+//        }
+//
+//        if(skills==null){
+//            model.addAttribute("title", "All Jobs");
+//            model.addAttribute("jobs", jobRepository.findAll());
+//            return "add";
+//        } else {
+//            List<Skill> skillObjs = (List<Skill>) skillRepository.findAllById(skills);
+//            if(skillObjs.isEmpty()){
+//                model.addAttribute("title", "Invalid Skill ID: "+ skills);
+//            } else{
+//                newJob.setSkills(skillObjs);
+//                model.addAttribute("title", "Jobs with Skills: " + skillObjs);
+//                model.addAttribute("jobs", newJob.getSkills());
+//            }
+//        }
         List<Skill> skillObjs = (List<Skill>) skillRepository.findAllById(skills);
         newJob.setSkills(skillObjs);
+        Employer employer = employerRepository.findById(employerId).orElse(new Employer());
+        newJob.setEmployer(employer);
 
-        Optional<Employer> employerObj = employerRepository.findById(employerId);
-        if(employerObj.isPresent()){
-            Employer employer = employerObj.get();
-            newJob.setEmployer(employer);
-        }
         jobRepository.save(newJob);
-
-//        employerRepository.save(newJob.getEmployer());
-//        skillRepository.save(newJob.getSkills());
-//        model.addAttribute("employers", jobRepository.findById(employerId));
-//        model.addAttribute("skills", jobRepository.findAllById(skills));
-//            if(employer.getJobs().contains(newJob)){
-//                newJob.setEmployer(employer);
-//                jobRepository.save(newJob);
-
-
         return "redirect:";
     }
-}
 
-//    @GetMapping("view/{jobId}")
-//    public String displayViewJob(Model model, @PathVariable int jobId) {
-//        Optional result = jobRepository.findById(jobId);
-//        if(result.isPresent()){
-//            Job job = (Job) result.get();
-//            model.addAttribute("job", job);
-//            return "view/{jobId}";
-//        }else {
-//            return "redirect:../";
-//        }
-//    }
-//}
+    @GetMapping("view/{jobId}")
+    public String displayViewJob(Model model, @PathVariable int jobId) {
+        Optional result = jobRepository.findById(jobId);
+        if(result.isPresent()){
+            Job job = (Job) result.get();
+            model.addAttribute("job", job);
+            return "view/{jobId}";
+        }else {
+            return "redirect:../";
+        }
+    }
+}
